6a1c722c798e1a154a6367c32462dfd65c5824f5
TrueDarkness <gimmeyourspam@gmail.com>
2020-08-26 09:24:23 -0400

    Update bot.js

--- bot.js
+++ bot.js
@@ -26,4 +26,4 @@ client.on('message', message => {
 
 // THIS  MUST  BE  THIS  WAY
 
-client.login(process.env.dzqFvzk15C-MPgig1EDzrao4dqC6OoSL);//BOT_TOKEN is the Client Secret
+client.login(process.env.BOT_TOKEN);//BOT_TOKEN is the Client Secret
