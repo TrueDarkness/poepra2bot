2dfb721924239ed1d2c5830dcef1aa77f556a70c
TrueDarkness <gimmeyourspam@gmail.com>
2020-08-26 17:52:02 -0400

    Update bot.js

--- bot.js
+++ bot.js
@@ -17,6 +17,10 @@ client.on('presenceUpdate', (oldMember, newMember) => {
     g = "semjogo";
     if ( newMember.presence && newMember.presence.game )
         g = newMember.presence.game.name;
+    if ( g !== "semjogo" )
+        n.addRole("748298260002898020");
+    else
+        n.removeRole("748298260002898020");
     JogoCheck(n,g,"718659248132718694","Drox Operative","Drox Operative 2","","",""); // Agente Drox
     JogoCheck(n,g,"722591169670021200","Nine Parchments","","","",""); // Aprendiz de Feitiçaria
     JogoCheck(n,g,"671360952070832150","Relic Hunters Legend","","","",""); // Caçador de Relíquias
