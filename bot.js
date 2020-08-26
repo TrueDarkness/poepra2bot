1c85841d65bc6f51c15d902699ea8367a9484196
TrueDarkness <gimmeyourspam@gmail.com>
2020-08-26 11:09:21 -0400

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,10 +1,6 @@
 const Discord = require('discord.js');
 const client = new Discord.Client();
 
-client.on('ready', async client => {
-    setInterval(await statuscheck(client), 10000); // Checar usuários a cada 10 segundos
-});
-
 client.on('message', message => {
     if (message.content === 'ping') {
        message.reply('pong!23');
@@ -15,30 +11,18 @@ client.on('message', message => {
 client.on('guildMemberAdd', member => {
 });
 
+// Mudança de Presença/Status
 client.on('presenceUpdate', (oldMember, newMember) => {
-    if ( newMember.presence.game.name === "Factorio" )
-        newMember.addRole('671361855574245377');
-    else
-        newMember.removeRole('671361855574245377');
+    n = newMember;
+    g = newMember.presence.game.name;
+    JogoCheck(n,g,"Factorio",671361855574245377);
 });
 
-// Checar usuários
-async function statuscheck() {
-    const statusArray = {};
-    await client.guilds.array().forEach(async g => {
-        const status = [];
-        await g.members.array().forEach(m => {
-            if (m.user.presence.game.name === "Factorio")
-                
-                m.addRole('671361855574245377');
-            else
-                m.addRole('671361855574245377');
-                m.removeRole('671361855574245377');
-        });
-        statusArray[g.id] = status;
-    });
-    console.log('set'); // /So I know the timer works
-    return statusArray;
+function JogoCheck(membroatual,jogoatual,nomejogo,rolejogo) {
+    if ( jogoatual === nomejogo )
+        membroatual.addRole(rolejogo);
+    else
+        membroatual.removeRole(rolejogo);
 }
 
 client.login(process.env.BOT_TOKEN);//BOT_TOKEN is the Client Secret
