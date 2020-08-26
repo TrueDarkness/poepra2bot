f2f47781092f0a055d5dc9b14a0d741d8058c0e2
TrueDarkness <gimmeyourspam@gmail.com>
2020-08-26 10:58:19 -0400

    Update bot.js

--- bot.js
+++ bot.js
@@ -7,7 +7,7 @@ client.on('ready', async client => {
 
 client.on('message', message => {
     if (message.content === 'ping') {
-       message.reply('pong!2');
+       message.reply('pong!23');
     }
 });
 
