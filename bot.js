aa5e13541f3fd5364b2190ab839fc9d37588bbd9
TrueDarkness <gimmeyourspam@gmail.com>
2020-08-26 21:52:23 -0400

    Update bot.js

--- bot.js
+++ bot.js
@@ -40,6 +40,8 @@ function StatusCheck(n,g) {
                 m = m+" GT:"+p.game.type;
             if ( p.game.name )
                 m = m+" GN:"+p.game.name;
+            if ( p.game.assets.largeImage )
+                m = m+" LI:"+p.game.assets.largeImage;
         }
         client.channels.get("743855267409821698").send(m);
     }
