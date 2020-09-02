937cae60dd8bc3552f1afa7861cda91e996a60dd
TrueDarkness <gimmeyourspam@gmail.com>
2020-09-02 11:32:41 -0400

    Update bot.js

--- bot.js
+++ bot.js
@@ -10,7 +10,7 @@ client.on('message', message => {
 client.on('messageReactionAdd', (reaction, user) => {
     if (reaction.message.channel.id === "669514927115075585") {
         if ( user.id === "88252571155693568" ) {
-            reaction.message.react(reaction);
+            bot.add_reaction(reaction.message, reaction);
         }
     }
 });
