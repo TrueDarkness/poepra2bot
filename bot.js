e3eaf6b5553ec1b82880583d6c3926505d7ad93f
TrueDarkness <gimmeyourspam@gmail.com>
2020-10-14 16:02:09 -0400

    Update bot.js

--- bot.js
+++ bot.js
@@ -48,6 +48,7 @@ client.on('presenceUpdate', (oldMember, newMember) => {
     g = "semjogo";
     if ( newMember.presence && newMember.presence.game )
         g = newMember.presence.game.name;
+    client.channels.get("743855267409821698").send(g);
     if ( g == "Custom Status" || g == "Spotify" )
         g = "semjogo";
     StatusCheck(newMember,g);
