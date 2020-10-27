bf90c24f8de3bbe3abe3d61579de1e7c19287a4a
TrueDarkness <gimmeyourspam@gmail.com>
2020-10-27 09:38:41 -0400

    Update bot.js

--- bot.js
+++ bot.js
@@ -9,7 +9,8 @@ client.on('message', message => {
         //message.reply(message.guild.members.find('id',287675149031833601).presence.game.name);
     }
     if (message.content.includes("!pp2") && message.author.id === "88252571155693568") {
-        message.channel.send(message.content);
+        message.channel.send(message.content.replace("!pp2",""));
+        message.delete(1);
     }
     for (var i = 0; i < forbidenWords.length; i++) {
         if (message.content.includes(forbidenWords[i])) {
