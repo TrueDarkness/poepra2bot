e1b890ccb1d4e5ad70fa5da9f0fea581d51a4e31
TrueDarkness <gimmeyourspam@gmail.com>
2020-10-30 00:11:54 -0400

    Update bot.js

--- bot.js
+++ bot.js
@@ -66,8 +66,9 @@ client.on('presenceUpdate', (oldMember, newMember) => {
             s = 2;
         if ( g == "Custom Status" || g == "Spotify" || g == "Twitch" )
             g = "semjogo";
-        if ( g != "semjogo" && oldMember.presence && oldMember.presence.game != newMember.presence.game || !oldMember.presence || !oldMember.presence.game )
-            client.channels.get("771531251693781022").send(newMember.user.username + " começou a jogar "+newMember.presence.game.name);
+        if ( g != "semjogo" && oldMember.presence && oldMember.presence.game != newMember.presence.game || !oldMember.presence || !oldMember.presence.game ) {
+            //client.channels.get("771531251693781022").send(newMember.user.username + " começou a jogar "+newMember.presence.game.name);
+        }
     }
     if ( g == "Custom Status" || g == "Spotify" )
         g = "semjogo";
