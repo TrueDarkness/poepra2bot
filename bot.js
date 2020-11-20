346a1386e087055b6e4435ae834e59e200981756
TrueDarkness <gimmeyourspam@gmail.com>
2020-11-19 22:34:49 -0400

    Update bot.js

--- bot.js
+++ bot.js
@@ -75,7 +75,7 @@ client.on('presenceUpdate', (oldMember, newMember) => {
     if ( o != g || s == 2 )
         StatusCheck(newMember,g,s);
     if ( o != g && g != "semjogo" ) {
-        client.channels.get("773764984923750453").send("**" + newMember.user.username + "** começou a jogar **" + g + "**");
+//        client.channels.get("773764984923750453").send("**" + newMember.user.username + "** começou a jogar **" + g + "**");
         //client.channels.get("773764984923750453").send(newMember.user.toString() + " começou a jogar " + g,{"allowedMentions": { "users" : []}})
     }
 });
