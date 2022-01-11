7c1af0a1c773037b8e005c847803d5a28bdb40ce
TrueDarkness <gimmeyourspam@gmail.com>
2022-01-11 11:44:02 -0400

    Update bot.js

--- bot.js
+++ bot.js
@@ -19,7 +19,7 @@ client.on('message', message => {
     }
     for (var i = 0; i < forbidenWords.length; i++) {
         if (message.content.includes(forbidenWords[i]) && message.author.id !== "740204208006889503") {
-            client.channels.get("743855267409821698").send("**" + message.author.username + "** : **" + message.content + "**");
+            client.channels.get("743855267409821698").send("Exclusão: **" + message.author.username + "**: " + message.content);
             message.delete(1)
             break;
         }
