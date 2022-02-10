845d6023bd1027ce9a50602a4d493108caccd32c
TrueDarkness <gimmeyourspam@gmail.com>
2022-02-09 22:30:47 -0400

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,6 +1,6 @@
 const Discord = require('discord.js'); 
 const client = new Discord.Client(); 
-
+ 
 forbidenGames = ["Custom Status", "Spotify", "Twitch", "Guilded", "Cyberpunk 2077", "Google Chrome", "Mir4G[1]", "Visual Studio Code"];
 forbidenWords = ["twitch.tv", "discord.com", "discord.gg", "@PS Updates", "@Switch Updates", "@XB Updates","@everyone"];
 
