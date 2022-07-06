68430be99bb240ed1f5a20bcbb0515bc96b53f92
TrueDarkness <gimmeyourspam@gmail.com>
2022-07-06 09:52:37 -0400

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,4 +1,4 @@
-const Discord = require('discord.js');
+const Discord = require('discord.js'); 
 const client = new Discord.Client();
  
 forbidenGames = ["Custom Status", "Spotify", "Twitch", "Guilded", "Cyberpunk 2077", "Google Chrome", "Mir4G[1]", "Visual Studio Code"];
